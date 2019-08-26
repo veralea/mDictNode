9bf3760888e7b919551afe8fcac6980b257f20f7
veralea <veralea@rambler.ru>
2019-08-26 14:20:43 +0300

    260819 2

---
+++ public/precache-manifest.42005c036e45fcd3dd7064c4f47e0146.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ab08b1a53b44ad00f8946daa52c692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4894e8d489734589241",
+    "url": "/static/css/main.609d98cf.chunk.css"
+  },
+  {
+    "revision": "20e9263a7a82bb4b71ab",
+    "url": "/static/js/2.7a2d2117.chunk.js"
+  },
+  {
+    "revision": "b4894e8d489734589241",
+    "url": "/static/js/main.f00c46a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
